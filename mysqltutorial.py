da4347593eacf9bc6f3e1d10ee75908967537757
liangk12 <kenneth.liang12@gmail.com>
2015-04-21 00:45:47 -0400

    having permissions issues (?) with the mysql writing to .pid. Working on this bugger

--- mysqltutorial.py
+++ mysqltutorial.py
@@ -5,6 +5,7 @@ http://www.newthinktank.com/2014/08/mysql-video-tutorial/
 
 import sqlalchemy
 
+# be sure to turn on msql.server
 mysqlengine=sqlalchemy.create_engine("mysql://user:password@localhost/foo")
 mysqlengine.execute("CREATE DATABASE IF NOT EXISTS test3;")
 # mysqlengine.execute("USE test3")
